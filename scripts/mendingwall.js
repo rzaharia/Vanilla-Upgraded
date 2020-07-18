edb901c0b2a8113ac1de4eb70906b5245123bdb2
Mesokrix <mesokrix@gmail.com>
2020-07-18 09:27:55 +0200

    cleanup mending walls codes

--- scripts/mendingwall.js
+++ scripts/mendingwall.js
@@ -1,8 +1,6 @@
 const cooldown1 = 30;
 const cooldown2 = 20;
 
-//ajouter des effets (2)(pour les deux mur)
-//effet de Thorge expension pour le test puis je modif après 
 const EffetPetit = newEffect(60, e => {
   Draw.color(Color.valueOf("6dff5d"), Color.valueOf("47a73d"), e.fslope());
     Draw.alpha(e.fout() * 0.5)
@@ -21,28 +19,54 @@ const EffetGrand = newEffect(60, e => {
     Draw.blend();
 });
 
-//Petit mur
-const mendingWall = extendContent
-  (Block, "mendingwall", {
+const mendingWall = extendContent(Block, "mendingwall", {
+    init(){
+      this.requirements(
+        Category.defense, 
+        ItemStack.with(
+          Vars.content.getByName(ContentType.item, "dev-obsidienne"), 12,
+          Vars.content.getByName(ContentType.item, "dev-emeraude"), 6
+        )
+      );
+    this.super$init();
+    },
+    
     update(tile){
-      //quand sa s'active
-      if((tile.entity.health < 390 && tile.entity.timer.get(0, cooldown2))){
-        //gagne la vie
+      if((tile.entity.health < 391 && tile.entity.timer.get(0, cooldown2))){
         tile.entity.health += 12;
         Effects.effect(EffetPetit, tile);
       }
     }
 });
+mendingWall.size = 1;
+mendingWall.health = 420;
+mendingWall.update = true;
+mendingWall.solid = true;
+mendingWall.blockGround = walls;
+TechTree.create(Blocks.titaniumWall, mendingWall);
 
-//Grand mur
-const mendingWallLarge = extendContent
-  (Block, "mendingwalllarge", {
+const mendingWallLarge = extendContent(Block, "mendingwalllarge", {
+    init(){
+      this.requirements(
+        Category.defense, 
+        ItemStack.with(
+          Vars.content.getByName(ContentType.item, "dev-obsidienne"), 24,
+          Vars.content.getByName(ContentType.item, "dev-emeraude"), 12
+        )
+      );
+    this.super$init();
+    },
+    
     update(tile){
-      //quand sa s'active
-      if((tile.entity.health < 1680 && tile.entity.timer.get(0, cooldown1))){
-        //gagne la vie
+      if((tile.entity.health < 1681 && tile.entity.timer.get(0, cooldown1))){
         tile.entity.health += 24 
         Effects.effect(EffetGrand, tile)
       }
     } 
 });
+mendingWallLarge.size = 2;
+mendingWallLarge.health = 1680;
+mendingWallLarge.update = true;
+mendingWallLarge.solid = true;
+mendingWallLarge.blockGround = walls;
+TechTree.create(mendingwalllarge, mendingwallLarge);
