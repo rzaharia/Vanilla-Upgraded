8798d5bcbc42d849e4727fa3c3d18e24d83431e8
VozDuh <61590217+Voz-Duh@users.noreply.github.com>
2020-08-08 21:08:11 +0300

    Hermetic Conveyor and Conduit

---
+++ scripts/extra/conv.js
@@ -0,0 +1,34 @@
+const conveyor = extendContent(Conveyor, "hermetique-conveyor", {
+	load(){
+		this.super$load();
+		this.topRegion = Core.atlas.find(this.name + "-top");
+	},
+	unitOn(){},
+	drawLayer(tile){
+		this.super$drawLayer(tile);
+		Draw.rect(this.topRegion, tile.drawx(), tile.drawy());
+    },
+    generateIcons: function(){
+	return [
+		Core.atlas.find(this.name),
+		Core.atlas.find(this.name + "-top")
+	];}
+});
+
+const conduit = extendContent(Conduit, "hermetique-conduit", {
+	load(){
+
+        this.liquidRegion = Core.atlas.find("conduit-liquid");
+        for(var i = 0; i < 7; i++){
+            this.topRegions[i] = Core.atlas.find(this.name + "-top-" + i);
+            this.botRegions[i] = Core.atlas.find(this.name + "-bottom-" + i);
+        }
+        this.glassRegion = Core.atlas.find("vanilla-upgraded-hermetique-conveyor-top");
+    },
+    draw(tile){
+    	this.super$draw(tile);
+    	Draw.rect(this.glassRegion, tile.drawx(), tile.drawy());
+    }
+});
+conduit.topRegions = [7];
+conduit.botRegions = [7];
