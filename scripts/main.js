cb109c03212a6606cb68c0cde25f5176cef9f36d
Mesokrix <mesokrix@gmail.com>
2020-08-13 00:41:43 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,51 +1,49 @@
-if(typeof(require) !== "undefined"){
-  //turret
-  require("turret/canson");
-  require("turret/rct-launcher");
-  require("turret/volcano");
-  require("turret/endduo");
-  require("turret/tourelledelabres");
-  require("turret/blizzard");
-  require("turret/sniper");
-  require("turret/bigfuse");
-  require("turret/grabomb");
-  require("turret/plasma");
-  require("turret/quantum");
-  require("turret/neutron");
-  //units
-  require("units/explocryo");
-  require("units/bigheal");
-  require("units/haos");
-  require("units/scorpio");
-  require("units/crounder");
-  require("units/renit");
-  require("units/misunit");
-  //mechs
-  require("mechs/anti");
-  require("mechs/mechdown");
-  require("mechs/envahisseur");
-  require("mechs/thunder");
-  require("mechs/glare");
-  require("mechs/builder");
-  //power
-  require("power/surge-tower-huge");
-  //walls
-  require("walls/mendingwall");
-  //production
-  require("production/am");
-  require("production/dd");
-  require("production/com");
-  require("production/grif");
-  require("production/vib");
-  //extra
-  require("extra/shield");
-  require("extra/electromine");
-  require("extra/soundmine");
-  //rest
-  require("rest/relief");
-  require("rest/funclib");
-  require("rest/effectlib");
-  require("rest/multilib");
-  //no replace
-  require("extra/conv");
-}
+//turret
+require("turret/canson");
+require("turret/rct-launcher");
+require("turret/volcano");
+require("turret/endduo");
+require("turret/tourelledelabres");
+require("turret/blizzard");
+require("turret/sniper");
+require("turret/bigfuse");
+require("turret/grabomb");
+require("turret/plasma");
+require("turret/quantum");
+require("turret/neutron");
+//units
+require("units/explocryo");
+require("units/bigheal");
+require("units/haos");
+require("units/scorpio");
+require("units/crounder");
+require("units/renit");
+require("units/misunit");
+//mechs
+require("mechs/anti");
+require("mechs/mechdown");
+require("mechs/envahisseur");
+require("mechs/thunder");
+require("mechs/glare");
+require("mechs/builder");
+//power
+require("power/surge-tower-huge");
+//walls
+require("walls/mendingwall");
+//production
+require("production/am");
+require("production/dd");
+require("production/com");
+require("production/grif");
+require("production/vib");
+//extra
+require("extra/shield");
+require("extra/electromine");
+require("extra/soundmine");
+//rest
+require("rest/relief");
+require("rest/funclib");
+require("rest/effectlib");
+require("rest/multilib");
+//no replace
+require("extra/conv");
