95202029de27365081a094c92447d6e04756e1eb
Mesokrix <mesokrix@gmail.com>
2020-11-22 11:53:02 +0100

    Fix bug and convert hjson to json

--- scripts/rest/effectlib.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	newEffectWDraw(lifetime, drawDst, renderer){
-		return new Effects.Effect(lifetime, drawDst, new Effects.EffectRenderer({render: renderer}));
-	},
-	
-	newGroundEffect(lifetime, staticLife, renderer){
-		return new GroundEffectEntity.GroundEffect(lifetime, staticLife, new Effects.EffectRenderer({render: renderer}));
-	}
-}
